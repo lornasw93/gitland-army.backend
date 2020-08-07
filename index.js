eb924d1443e8ae5b523e90b09d1ef062eade777f
Lorna <lorna.watson@epiphany.co.uk>
2020-08-07 02:59:46 +0100

    removed duplicates

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ var server = http.Server(app);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors());
-
 var PORT = process.env.PORT || 5000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.use(cors());
 
