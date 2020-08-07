6406489be2c7881d461f32e6c12b481d473fb5ea
Lorna <lorna.watson@epiphany.co.uk>
2020-08-07 02:49:00 +0100

    functions added

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 var express = require("express");
-const app = express();
 
-var http = require('http');
+const app = express();
+var cors = require("cors"),
+  bodyParser = require("body-parser");
+
+var http = require("http");
 var server = http.Server(app);
+ 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
 
 var PORT = process.env.PORT || 5000;
 
-app.get("/", (request, res) => {
-   res.send('Hello world!');
-  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-server.listen(PORT, function(){
+app.use(cors());
 
-console.log('Running');
+app.get("/warhead/status", (request, res) => {
+  var textArray = ["online", "offline"];
+  var randomNumber = Math.floor(Math.random() * textArray.length);
+  var result = textArray[randomNumber];
 
+  console.log(`status result: ${result}`);
+
+  res.send(result);
+});
+
+app.post("/warhead/launch/:code", (request, res) => {
+  var date = "200807"; //new Date().toISOString("yyMMdd");
+  var secret = "NICEGAMEOFCHESS";
+
+  //console.log(request.body.code);
+
+  if (request.body.code === `${date}${secret}`) {
+    res.status(200).send({
+      message: "success",
+    });
+  } else {
+    response.status(400).send({
+      message: "failure",
+    });
+  }
+});
+
+server.listen(PORT, function () {
+  console.log(`Running on port ${PORT}`);
 });
